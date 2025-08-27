a5abfc1ba105449242dc763e1b44c44e87ea7d78
seba.pavlovic <seba.pavlovic.pp@gmail.com>
2025-08-26 23:37:06 -0400

    asteroids commit 2

--- main.py
+++ main.py
@@ -1,22 +1,39 @@
 import pygame
 from constants import *
+from player import Player
 
 def main():
+    #Inicializa Pygame
     pygame.init()
     
     print("Starting Asteroids!")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
     
+    #Genera la ventana en donde se verá el juego, y asigna sus dimensiones según las variables importadas
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-    black = (0, 0, 0)
+    #Genera el objeto "clock" que lleva registro del tiempo transcurrido
+    clock = pygame.time.Clock()
+    #Guarda el delta time, que guarda el tiempo que ha pasado desde el ultimo frame
+    dt = 0
+    #Crea una instancia de jugador y la centra en la pantalla
+    player = Player((SCREEN_WIDTH / 2), (SCREEN_HEIGHT / 2), PLAYER_RADIUS)
 
     while True:
+        #Cierra la ventana y termina el loop si se apreta el boton X
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        screen.fill(black)
+        
+        #Hace que la pantalla se coloree de negro cada frame
+        screen.fill("black")
+        #Dibuja al jugador en la pantalla
+        player.draw(screen)
+        #Actualiza el contenido de toda la ventana de display
         pygame.display.flip()
+
+        #limitar frames a 60 FPS
+        dt = clock.tick(60) / 1000
+        
+    
         
 
 if __name__ == "__main__":
